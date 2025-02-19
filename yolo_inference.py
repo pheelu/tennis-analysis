db2e76ad17f791d38b845869826d299b092e136c
Filippo Alzati <filippoprivata@gmail.com>
2025-02-19 19:00:55 +0100

    keypoints dataset

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
-model = YOLO('models/yolov5_last.pt')  # Load model``
+model = YOLO('yolov8x')  # Load model``
 
-result = model.predict('input_videos/input_video.mp4', conf=0.2, save=True)
+result = model.track('input_videos/input_video.mp4', conf=0.2, save=True)
 
-print(result)
-print("boxes:")
+# print(result)
+# print("boxes:")
 
-for box in result[0].boxes:
-    print(box)
+# for box in result[0].boxes:
+#     print(box)
