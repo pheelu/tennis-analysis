d0ad0d1fe5c782e1d22a3a1efc3cd7e40d70f77f
Filippo Alzati <filippoprivata@gmail.com>
2025-02-22 22:01:29 +0100

    player_tracker.py test

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from utils import (read_video, 
                    save_video)
 
+from trackers import PlayerTracker
+
 def main():
+    # Read video frames
     input_video_path = 'input_videos/input_video.mp4'
     video_frames = read_video(input_video_path)
+
+
+    # Detect players in the video
+    player_tracker = PlayerTracker('yolov8x.pt')
+    player_detections = player_tracker.detect_frames(video_frames)
+
+    # Draw bounding boxes around players
+    ## Draw players bboxes
+    output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
 
     save_video(video_frames, 'output_videos/output_video.avi')
 
